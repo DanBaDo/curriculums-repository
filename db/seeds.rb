db57b17e73cc014b3acc830ba5d9fc468823a733
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2023-07-16 18:40:53 +0200

    Provides seeds for models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+UserGroup.destroy_all
+User.destroy_all
+groups = UserGroup.create([{description: "admin"}, {description: "candidate"}, {description: "staff"}])
+daniel = User.create(email: "daniel@email.com")
+daniel.user_groups = [groups[0],groups[2]]
+daniel.save()
+User.create(email: "maria@eamil.com", user_groups: [groups[1]])
