ac060433da2af32c8f0674918a777691740567bc
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2023-07-18 23:33:02 +0200

    Uses email verification sender in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,14 @@
 class UsersController < ApplicationController
 
-    #require 'mailjet'
-        
+    include SendMail
+
     def new
 
     end
 
     def create
         @article = User.create(params.require(:users).permit(:email))
-        variable = Mailjet::Send.create(messages: [{
-            'From'=> {
-              'Email'=> 'bytes4life@proton.me',
-              'Name'=> 'Teu Curriculum'
-            },
-            'To'=> [
-              {
-                'Email'=> params[:users][:email],
-                'Name'=> 'Novo usuario'
-              }
-            ],
-            'Subject'=> 'Teu Curriculum: confirma o rexistro',
-            'TextPart'=> 'Aquí irá o enlace de confirmación',
-            'HTMLPart'=> '<h3>Benvido a Teu Curriculum</h3><p>Aquí irá o <a>enlace</a> de confirmación.</p>',
-            #'CustomID' => ''
-          }]
-        )
-        p variable.attributes['Messages']
+        send_email_validation(params[:users][:email])
         redirect_to verify_your_email_path
     end
 
