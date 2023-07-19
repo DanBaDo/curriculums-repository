4aafda96ad3487719ddfa8a47afca5edaa2cf610
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2023-07-19 02:18:00 +0200

    Includes validation URL in mails

--- lib/send_mail.rb
+++ lib/send_mail.rb
@@ -1,24 +1,43 @@
 module SendMail
-    def send_email_validation(email)
-      # Abstraction for sending email validation URL
-      # @param [String] email
-      # @return [void]
-      delivery = Mailjet::Send.create(messages: [{
-          'From'=> {
-            'Name'=> 'Teu Curriculum',
-            'Email'=> 'bytes4life@proton.me',
-          },
-          'To'=> [
-            {
-              'Email'=> email,
-              'Name'=> 'Novo usuario'
-            }
-          ],
-          'Subject'=> 'Teu Curriculum: confirma o teu rexistro',
-          'TextPart'=> 'Aquí irá o enlace de confirmación',
-          'HTMLPart'=> '<h3>Benvido a Teu Curriculum</h3><p>Aquí irá o <a>enlace</a> de confirmación.</p>',
-        }]
-      )
-      p delivery.attributes['Messages'] if Rails.env.development?
-    end
+
+  include JwtProvider
+
+  def send_email_validation(email)
+    # Abstraction for sending email validation URL
+    # @param [String] email
+    # @return [void]
+    jwt = new_email_validation_jwt_for email
+    email_validation_url = request.base_url+email_validation_path+"?jwt="+jwt
+    delivery = Mailjet::Send.create(messages: [{
+      'From': {
+        'Name'=> 'Teu Curriculum',
+        'Email'=> 'bytes4life@proton.me',
+      },
+      'To'=> [
+        {
+          'Email'=> email,
+          'Name'=> 'Novo usuario'
+        }
+      ],
+      'Subject'=> 'Teu Curriculum: confirma o teu rexistro',
+      'TextPart'=> "
+Se acabas de darte de alta no noso servicio confirma a túa alta
+accedendo a este enlace de verificación:
+#{email_validation_url}.
+Se non fuches tí quen se deu de alta, non te preocupes. Non tes que facer nada.
+",
+      'HTMLPart'=> "
+<h3>Benvido a Teu Curriculum</h3>
+<p>
+  Se acabas de darte de alta no noso servicio cnafirma a túa alta
+  accedendo a este <a href=\"#{email_validation_url}\">enlace de verificación</a>.
+</p>
+<p>
+  Se non fuches tí quen se deu de alta, non te preocupes. Non tes que facer nada.
+</p>
+",
+    }])
+    p delivery.attributes['Messages'] if Rails.env.development?
+  end
+
 end
